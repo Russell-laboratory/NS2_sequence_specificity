c33e1edc7e14b00c964ae52e4a478f71cf4d6869
Alistair B Russell <a5russell@tscc-1-38.tscc.sdsc.edu>
2024-09-02 15:51:23 -0700

    getting ready to publish soon

--- Scripts/.ipynb_checkpoints/mutCalling-checkpoint.py
+++ Scripts/.ipynb_checkpoints/mutCalling-checkpoint.py
@@ -199,7 +199,108 @@ def positionalDataTrim(*, sortedBamfile, outfile, qvalCutoff, ignorePos):
                     outfile.write(writeline)
         arguments = f"rm -f {tempFile}"
 
-
+#quick and dirty to handle caps before I write something better that takes it pre-alignemnt
+def positionalDataIgnoreInsert(*, sortedBamfile, outfile, qvalCutoff, insertionCutoff):
+    tempFile = sortedBamfile.split('.')[0] + '_temp.sam'
+    output = {}
+    command  = ' '.join(['samtools view -f 2 -F 524', sortedBamfile,">", tempFile])
+    os.system(command)
+    with open(tempFile, 'r') as infile:
+        while True:
+            line1 = infile.readline()
+            line2 = infile.readline()
+            if not line2:
+                break
+            firstMate = line1.split('\t')
+            secondMate = line2.split('\t')
+            if firstMate[0] != secondMate[0]:
+                print('Odd number of matepairs in file...fix')
+                break
+            #for each, store mapped positions (and small deletions, not counting them) in one list, and deletions in another. Then compare.
+            #ignore chimeras
+            if firstMate[2] == secondMate[2]:
+                currentSeq = {}
+                insertions ={}
+                segment = firstMate[2]
+                insertionTooLarge = False
+                for read in [firstMate, secondMate]:
+                    CIGAR = read[5]
+                    currentPosSeq = int(read[3])
+                    score = read[10]
+                    sequence = read[9]
+                    #start positions and mapping. Just go with lowest score at position rather than handling in a Bayesian framework. There is a lot of discussion about that.
+                    CIGARval = []
+                    curstring = ''
+                    for character in CIGAR:
+                        if not character.isalpha():
+                            curstring += character
+                        else:
+                            if character == 'I':
+                                if int(curstring) >= insertionCutoff:
+                                    insertionTooLarge = True
+                            CIGARval.append((int(curstring), character))
+                            curstring = ''
+                    currentPosRead = 1
+                    #For D, N, advance position in sequence but not read. For I, S, advance position in read but not sequence
+                    #For M, add in
+                    for value in CIGARval:
+                        if value[1] == 'M':
+                            #add all mapped positions to a list
+                            basesAdded = 0
+                            while basesAdded < value[0]:
+                                if currentPosSeq not in currentSeq or ord(score[currentPosRead-1]) - 33 > currentSeq[currentPosSeq][1] or currentSeq[currentPosSeq] == 'D':
+                                    currentSeq[currentPosSeq] = (sequence[currentPosRead-1], ord(score[currentPosRead-1]) - 33)
+                                basesAdded += 1
+                                currentPosSeq += 1
+                                currentPosRead +=1
+                        elif value[1] == 'D':
+                            basesAdded = 0
+                            #only count if quality score to either side meets standard. Only called if base not already called at position.
+                            if currentPosSeq not in currentSeq and ord(score[currentPosRead-2]) - 33 >= qvalCutoff and ord(score[currentPosRead]) - 33 >= qvalCutoff:
+                                while basesAdded < value[0]:
+                                    currentSeq[currentPosSeq] = ('D', qvalCutoff)
+                                    basesAdded += 1
+                                    currentPosSeq += 1
+                                    #do not advance place in read
+                            else:
+                                currentPosSeq += value[0]
+                        elif value[1] == 'N':
+                            currentPosSeq += value[0]
+                        #inserted sequence gets added at the position if bases before and after are a good score
+                        elif value[1] == 'I':
+                            if ord(score[currentPosRead-2]) - 33 >= qvalCutoff and ord(score[currentPosRead + value[0] - 1] ) - 33 >= qvalCutoff:
+                                insertions[currentPosSeq] = value[0]
+                                currentPosRead += value[0]
+                        elif value[1] == 'S':
+                            currentPosRead += value[0]
+                ignoreList = []
+                #now "trim" Use a tuple-keyed list of positions unique to each amplicon and then ignore positions defined in thing
+                if not insertionTooLarge:
+                    for position in currentSeq:
+                        if position not in ignoreList:
+                            if segment not in output:
+                                output[segment] = {}
+                            if position not in output[segment]:
+                                output[segment][position] = {'A':0, 'G':0,'C':0,'T':0, 'D':0, 'I':0}
+                            if currentSeq[position][1] >= qvalCutoff:
+                                #for any reasonable qValCutoff none should be N's, but just to be anal retentive
+                                base = currentSeq[position][0]
+                                if base != 'N':
+                                    output[segment][position][base] += 1
+                    for position in insertions:
+                        if position not in ignoreList:
+                            output[segment][position]['I'] += 1
+        with open(outfile, 'w') as outfile:
+            #might as well put it all in order in a tsv
+            outfile.write('\t'.join(['segment','position','A','G','C','T', 'D','I']) + '\n')
+            for segment in sorted(output.keys()):
+                for position in sorted(output[segment]):
+                    writeline = '\t'.join([segment, str(position)])
+                    for base in ['A','G','C','T','D','I']:
+                        writeline += '\t' + str(output[segment][position][base])
+                    writeline += '\n'
+                    outfile.write(writeline)
+        arguments = f"rm -f {tempFile}"
 
 #assumes sequence of interest is in R2, if not, invert
 def cappedExclude(*, searchSeqs, R1in, R2in, R1out,R2out, toleratedDifs):
@@ -331,6 +432,11 @@ def fastaReport(*, inFasta,outfile):
                 currSeq = ''
             else:
                 currSeq += line
+        for position, character in enumerate(currSeq):
+            position = position + 1
+            if position not in positions:
+                positions[position] = {'A':0, 'G':0,'C':0,'T':0}
+            positions[position][character] += 1
         outfile.write('\t'.join(['position','A','G','C','T']) + '\n')
         for position in positions:
             curr = positions[position]
